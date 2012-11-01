f84d8e587db56d83ad3b7fa877b64dc7d95522d4
Erik Rozendaal <erik@deler.org>
2012-11-01 11:44:04 +0100

    Database test.

--- src/test/java/com/zilverline/tdd/dao/JpaHibernateDaoTest.java
+++ src/test/java/com/zilverline/tdd/dao/JpaHibernateDaoTest.java
@@ -2,9 +2,6 @@ package com.zilverline.tdd.dao;
 
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -21,8 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.zilverline.tdd.config.PersistenceConfig;
 import com.zilverline.tdd.domain.InvestmentAccount;
-import com.zilverline.tdd.domain.Money;
-import com.zilverline.tdd.domain.Participant;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes={ TestDatabaseConfig.class, PersistenceConfig.class})
@@ -45,29 +40,7 @@ public class JpaHibernateDaoTest {
     }
 
     @Test
-    public void test_find_minimum_balance() {
-        Participant participant1 = givenParticipantWithBalance(Money.fromEuros(10));
-        Participant participant2 = givenParticipantWithBalance(Money.fromEuros(5));
-
-        List<Participant> actual = subject.findParticipantsWithMinimumBalanceOf(Money.fromEuros(8));
-
-        assertEquals(Arrays.asList(participant1), actual);
-    }
-
-    @Test
-    public void test_minimum_balance_zero() {
-        Participant participant = givenParticipantWithBalance(Money.ZERO);
-
-        List<Participant> actual = subject.findParticipantsWithMinimumBalanceOf(Money.ZERO);
-
-        assertEquals(Arrays.asList(participant), actual);
-    }
-
-    private Participant givenParticipantWithBalance(Money balance) {
-        Participant participant = new Participant("1233211234", 1);
-        investmentAccount.addParticipant(participant);
-        participant.addBalance(balance);
-        entityManager.persist(participant);
-        return participant;
+    public void todo() {
+        fail("todo");
     }
 }
