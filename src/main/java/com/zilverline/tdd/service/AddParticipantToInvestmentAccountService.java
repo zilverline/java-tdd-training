c78dc086425b2f8f2c1b6d218f5bc877df1bb829
Erik Rozendaal <erik@deler.org>
2012-11-01 12:16:52 +0100

    Mocking tests.

--- src/main/java/com/zilverline/tdd/service/AddParticipantToInvestmentAccountService.java
+++ src/main/java/com/zilverline/tdd/service/AddParticipantToInvestmentAccountService.java
@@ -1,14 +1,7 @@
 package com.zilverline.tdd.service;
 
-import java.util.Collection;
-
-import javax.inject.Inject;
-
-import org.springframework.transaction.annotation.Transactional;
-
 import com.zilverline.tdd.adapter.DueDilligenceParticipantService;
 import com.zilverline.tdd.domain.InvestmentAccount;
-import com.zilverline.tdd.domain.Liability;
 import com.zilverline.tdd.domain.Participant;
 import com.zilverline.tdd.domain.SignificantShareholderWithLiabiliesException;
 
@@ -16,20 +9,11 @@ public class AddParticipantToInvestmentAccountService {
 
     private final DueDilligenceParticipantService dueDilligenceParticipantService;
 
-    @Inject
     public AddParticipantToInvestmentAccountService(DueDilligenceParticipantService dueDilligenceParticipantService) {
         this.dueDilligenceParticipantService = dueDilligenceParticipantService;
     }
 
-    @Transactional(rollbackFor=Exception.class)
     public void execute(InvestmentAccount investmentAccount, Participant participant) throws SignificantShareholderWithLiabiliesException {
-        investmentAccount.addParticipant(participant);
-        if (participant.isSignificantShareholder()) {
-            Collection<Liability> liabilities = dueDilligenceParticipantService.dueDilligence(participant);
-            if (!liabilities.isEmpty()) {
-                investmentAccount.removeParticipant(participant);
-                throw new SignificantShareholderWithLiabiliesException(participant, liabilities);
-            }
-        }
+        throw new RuntimeException("TODO");
     }
 }
